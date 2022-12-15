8e184f7cc150ebff431daf739120ef4d1468b5eb
sp-ino <spinovale97@gmail.com>
2022-12-15 17:27:59 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='plottools',
-      version='0.0.0',
+      version='1.0.0',
       description='A set of command line scripts for plotting data with python',
       url='https://github.com/Sp-ino/plottools',
       author='Valerio Spinogatti',
       author_email='spinovale97@gmail.com',
       license='GNU',
-      packages=[],
-      scripts=['bin/basic_plot',
-               'bin/compute_fom_adc',
-               'bin/fourier',
-               'bin/histogr',
-               'bin/multi_axes_plot',
-               'bin/rms_discr'],
+      packages=find_packages(),
+      entry_points={'console_scripts' : ['basic_plot=plottools.basic_plot:main',
+                                    'compute_fom_adc=plottools.compute_fom_adc:main',
+                                    'fourier=plottools.fourier:main',
+                                    'histogr=plottools.histogr:main',
+                                    'multi_axes_plot=plottools.multi_axes_plot:main',
+                                    'rms_discr=plottools.rms_discr:main']},
+      install_requires=['matplotlib', 'numpy'],
       zip_safe=False)
