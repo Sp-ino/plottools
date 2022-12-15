e3c48c7c0a856573bdf1addbee5d640b551275bc
sp-ino <spinovale97@gmail.com>
2022-12-15 17:44:32 +0100

    Add useful reminder to setup.py

--- setup.py
+++ setup.py
@@ -16,3 +16,27 @@ setup(name='plottools',
                                     'rms_discr=plottools.rms_discr:main']},
       install_requires=['matplotlib', 'numpy'],
       zip_safe=False)
+
+
+# To use the scripts argument I should rename 'plottools' as 'scripts'
+# and use the following setup():
+# 
+# setup(name='plottools',
+#       version='1.0.0',
+#       description='A set of command line scripts for plotting data with python',
+#       url='https://github.com/Sp-ino/plottools',
+#       author='Valerio Spinogatti',
+#       author_email='spinovale97@gmail.com',
+#       license='GNU',
+#       scripts=['scripts/basic_plot.py',
+#             'scripts/compute_fom_adc.py',
+#             'scripts/fourier.py',
+#             'scripts/histogr.py',
+#             'scripts/multi_axes_plot.py',
+#            'scripts/rms_discr.py'],
+#       install_requires=['matplotlib', 'numpy'],
+#       zip_safe=False)
+# 
+# Note that also in this case IT WON'T WORK if the installation path
+# is not on PATH!
+
